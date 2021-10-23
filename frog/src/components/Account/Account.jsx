8601e3c4462da2561c104fdc19a76171c4c1bd6c
Александр <Fybeer@yandex.ru>
2021-10-23 22:53:32 +0300

    mooooooore components and work with props and routes (+ micro moments with Navlink)

--- frog/src/components/Account/Account.jsx
+++ frog/src/components/Account/Account.jsx
@@ -1,23 +1,23 @@
-import Style_account from './Account_style/Account.module.css'
+import Account_style from './Account_style/Account.module.css'
 import Posts from './Posts';
 
 const Account = () => {
-	return (
-		<div className={Style_account.box}>
-          <img src="https://phonoteka.org/uploads/posts/2021-04/1619464749_15-phonoteka_org-p-shapka-dlya-yutuba-chernii-fon-25.jpg" alt="" className={Style_account.header} />
-          <div className={Style_account.user}>
-            <img src="https://yt3.ggpht.com/ytc/AKedOLQDpJL4j3zA9P_m9NpSA9NV8Fp2LoRg8Gdjy-OW=s900-c-k-c0x00ffffff-no-rj" alt="" className={Style_account.avatar} />
-            <ul className={Style_account.info}>
-              <li className={Style_account.name}>Alexandr R.</li>
-              <li className={Style_account.item}>Date of Birth: 21 June</li>
-              <li className={Style_account.item}>City: Sevastopol</li>
-              <li className={Style_account.item}>Education: SevSU</li>
-              <li className={Style_account.item}>Web Site: https//...</li>
-            </ul>
-          </div>
-          <Posts></Posts>
-        </div>
-	);
+  return (
+    <div className={Account_style.box}>
+      <img src="https://phonoteka.org/uploads/posts/2021-04/1619464749_15-phonoteka_org-p-shapka-dlya-yutuba-chernii-fon-25.jpg" alt="" className={Account_style.header} />
+      <div className={Account_style.user}>
+        <img src="https://yt3.ggpht.com/ytc/AKedOLQDpJL4j3zA9P_m9NpSA9NV8Fp2LoRg8Gdjy-OW=s900-c-k-c0x00ffffff-no-rj" alt="" className={Account_style.avatar} />
+        <ul className={Account_style.info}>
+          <li className={Account_style.name}>Alexandr R.</li>
+          <li className={Account_style.item}>Date of Birth: 21 June</li>
+          <li className={Account_style.item}>City: Sevastopol</li>
+          <li className={Account_style.item}>Education: SevSU</li>
+          <li className={Account_style.item}>Web Site: https//...</li>
+        </ul>
+      </div>
+      <Posts></Posts>
+    </div>
+  );
 }
 
 export default Account;
--- frog/src/components/Account/Posts.jsx
+++ frog/src/components/Account/Posts.jsx
@@ -1,12 +1,20 @@
-import Style_posts from './Account_style/Posts.module.css'
+import Posts_style from './Account_style/Posts.module.css'
+import Post from './Post/Post';
 
 const Posts = () => {
 	return (
-		<div className={Style_posts.box}>
-			<p className={Style_posts.header}>My posts</p>
-			<textarea type="text" className={Style_posts.input}></textarea>
-			<button className={Style_posts.add}>send</button>
+		<div>
+			<div className={Posts_style.box}>
+			<p className={Posts_style.header}>My posts</p>
+			<textarea type="text" className={Posts_style.input}></textarea>
+			<button className={Posts_style.add}>send</button>
 		</div>
+		<Post message='Hi this is new web frog'></Post>
+		<Post message='This is messenger made in react'></Post>
+		<Post message='Take it'></Post>
+		<Post message='Thanks'></Post>
+		</div>
+		
 	);
 }
 
